b2e5661b9c09001f43479b2f05d214bc2135f538
Desire <asinguradesirecomfort@gmail.com>
2026-09-22 06:35:50 +0300

    refactor: move authentication into dedicated Nest module

---
+++ Backend/src/auth/auth.dto.ts
@@ -0,0 +1,1 @@
+import {IsEmail,IsString,MinLength} from 'class-validator';export class LoginDto{@IsEmail()email!:string;@IsString()@MinLength(8)password!:string}export class SignupDto extends LoginDto{@IsString()@MinLength(2)fullName!:string}
